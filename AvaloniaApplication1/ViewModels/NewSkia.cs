bbfcf2b8e3d8fbed343857aba2c38612f0285f82
BostonovOH <BostonovOH@bnipi.rosneft.ru>
2024-02-22 18:27:47 +0500

    Вынес выше объекты

--- AvaloniaApplication1/ViewModels/NewSkia.cs
+++ AvaloniaApplication1/ViewModels/NewSkia.cs
@@ -7,6 +7,7 @@ using Avalonia.Rendering;
 using Avalonia.Rendering.SceneGraph;
 using Avalonia.Skia;
 using Avalonia.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
@@ -19,32 +20,35 @@ namespace AvaloniaApplication1.ViewModels
 {
     public partial class NewSkia : Control
     {
+        IList<Device> devices = new List<Device>();
+        
         public NewSkia()
         {
+            var random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    var device = new Device()
+                    {
+                        Position = new Point(10 + i * 10, 10 + j * 10),
+                        Color = new SKColor((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255))
+                    };
+
+                    devices.Add(device);
+                }
+            }
         }
 
         class NewCustomDraw : ICustomDrawOperation
         {
-            IList<Device> devices = new List<Device>();
+            IEnumerable<Device> objectsToDraw;
 
-            Random random = new Random();
-            public NewCustomDraw(Rect bounds)
+            public NewCustomDraw(Rect bounds, IEnumerable<Device> objectsToDraw)
             {
-                Bounds = bounds;
-            
-                for (int i = 0; i < 100; i++)
-                {
-                    for (int j = 0; j < 100; j++)
-                    {
-                        var device = new Device()
-                        {
-                            Position = new Point(10 + i * 10, 10 + j * 10),
-                            Color = new SKColor((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255))
-                        };
-
-                        devices.Add(device);
-                    }                    
-                }                
+                this.objectsToDraw = objectsToDraw;
+                Bounds = bounds;                          
             }
 
             public Rect Bounds { get; }
@@ -65,7 +69,7 @@ namespace AvaloniaApplication1.ViewModels
                 var canvas = lease.SkCanvas;
                 canvas.Save();
 
-                foreach (var device in this.devices)
+                foreach (var device in this.objectsToDraw)
                 {
                     device.Draw(canvas);
                 }
@@ -76,7 +80,7 @@ namespace AvaloniaApplication1.ViewModels
 
         public override void Render(DrawingContext context)
         {
-            context.Custom(new NewCustomDraw(new Rect(0, 0, Bounds.Width, Bounds.Height)));
+            context.Custom(new NewCustomDraw(new Rect(0, 0, Bounds.Width, Bounds.Height), this.devices));
             Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
         }
     }
